470f58d58a1db9bdd228b463029a74a397eed9ed
novakvova <novakvova@gmail>
2025-03-27 16:48:53 +0200

    Add Database

---
+++ TestingSP/ThreadAndDatabase/DataBaseManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace ThreadAndDatabase
+{
+    public class DataBaseManager
+    {
+        private readonly ThreadAppContext _threadAppContext;
+
+        public DataBaseManager()
+        {
+            _threadAppContext = new ThreadAppContext();
+            
+        }
+    }
+}
---
+++ TestingSP/ThreadAndDatabase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace ThreadAndDatabase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start(); //Обчислюємо час роботи програми
+
+            
+            ThreadAppContext threadAppContext = new ThreadAppContext();
+            threadAppContext.Banans.Any();
+
+            stopwatch.Stop();
+            //TimeSpan - змінна, яка може зберігаати, сек, мілісенди, хв, год, дні
+            TimeSpan ts = stopwatch.Elapsed; //Оперує тіками - одиниці часу
+            Console.WriteLine($"Run time {ts}");
+        }
+    }
+}
