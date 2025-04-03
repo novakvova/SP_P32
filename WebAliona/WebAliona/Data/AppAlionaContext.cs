61b475184bd08ef25e3332b1d039e49037f5c69d
novakvova <novakvova@gmail>
2025-04-03 16:08:29 +0300

    Add new Project Testing Task

---
+++ WebAliona/WebAliona/Data/AppAlionaContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAliona.Data
+{
+    public class AppAlionaContext : DbContext
+    {
+        public AppAlionaContext(DbContextOptions<AppAlionaContext> options)
+            : base(options) { }
+
+        public DbSet<Banan> Banans { get; set; }
+    }
+}
