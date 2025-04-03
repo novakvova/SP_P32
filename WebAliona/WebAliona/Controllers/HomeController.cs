c59ae20919f231e809120ee7088aedc6e9219a5c
novakvova <novakvova@gmail>
2025-04-03 17:46:54 +0300

    View List Data

--- WebAliona/WebAliona/Controllers/HomeController.cs
+++ WebAliona/WebAliona/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAliona.Data;
 using WebAliona.Models;
 
 namespace WebAliona.Controllers
@@ -7,15 +9,35 @@ namespace WebAliona.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppAlionaContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            AppAlionaContext context)
         {
             _logger = logger;
+            _context = context;
+            if (!_context.Banans.Any()) 
+            {
+                DataBaseManager dbm = new DataBaseManager();
+                dbm.AddBanans(_context);
+            }
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            Console.WriteLine("Thread main id " + Thread.CurrentThread.ManagedThreadId);
+            List<Banan> list = await GetListBanansAsync();
+            //Task<List<Banan>> list = GetListBanansAsync();
+            return View(list);
+        }
+
+        private Task<List<Banan>> GetListBanansAsync()
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"Child Thread: {Thread.CurrentThread.ManagedThreadId}");
+                return _context.Banans.ToList();
+            });
         }
 
         public IActionResult Privacy()
---
+++ WebAliona/WebAliona/Data/DataBaseManager.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+
+namespace WebAliona.Data
+{
+    public class DataBaseManager
+    {
+        public void AddBanans(AppAlionaContext context)
+        {
+            var faker = new Faker<Banan>("uk")
+                .RuleFor(b => b.FirstName, f => f.Name.FirstName())
+                .RuleFor(b => b.LastName, f => f.Name.LastName())
+                .RuleFor(b => b.Image, f => f.Internet.Avatar())
+                .RuleFor(b => b.Phone, f => f.Phone.PhoneNumber())
+                .RuleFor(b => b.Sex, f => f.Random.Bool());
+
+            for (int i = 0; i < 20; i++)
+            {
+                var b = faker.Generate(1);
+                context.Add(b[0]);
+                context.SaveChanges();
+            }
+        }
+    }
+}
