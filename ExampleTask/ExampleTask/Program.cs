4f1d3b3b1c8d32fd12a1bf841a2f929b738be530
novakvova <novakvova@gmail>
2025-04-08 16:46:49 +0300

    Working task example

---
+++ ExampleTask/ExampleTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Привіт команда!");
+
+//Це метод, який нічого не віртає
+Task<int> task = new Task<int>(() =>
+{
+    Console.WriteLine("Привіт 1");
+    Console.WriteLine($"Номер потоку {Thread.CurrentThread.ManagedThreadId}");
+    Thread.Sleep(1000);
+    Console.WriteLine("Потік завершив свою роботу");
+    return 1;
+});
+
+task.Start(); //теж саме, що Task.Run
+//var result = task.Result; // Просто тормозить роботу потоку, поки не буде результату
+//task.Wait(); //Працює аналогічно task.Result - тормозить поточний потік
+
+int result = await task; //Очікує завершення задачі, томозить поточний потік, але
+//Воно виходить із методу і запамятовую коди має повенутися назад
+
+Console.WriteLine($"Основний потік папа {Thread.CurrentThread.ManagedThreadId}");
