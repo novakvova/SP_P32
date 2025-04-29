fa9610ce4b32eeda189e78b1f3603e12e9cf9e38
novakvova <novakvova@gmail>
2025-04-29 17:54:04 +0300

    Begin simple form

--- SPExamen/SPExamen/Models/ScanOptions.cs
+++ SPExamen/SPExamen/Models/ScanOptions.cs
@@ -10,5 +10,17 @@
         /// Папка куди буде збегігатися результат
         /// </summary>
         public string DirSave { get; set; } = String.Empty;
+        /// <summary>
+        /// Слова які ми шукаємо
+        /// </summary>
+        public string SearchWords { get; set; } = String.Empty;
+        /// <summary>
+        /// Якщо обрали файл
+        /// </summary>
+        public bool IsFile { get; set; }
+        /// <summary>
+        /// Файл, який ми обрали
+        /// </summary>
+        public IFormFile? FileWords { get; set; }
     }
 }
